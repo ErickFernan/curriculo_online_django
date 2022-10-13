2425ea4d41d2b8173a6f6cf94550d0571fbcccef
Erick <dverickfernan@gmail.com>
2022-10-13 19:15:46 -0300

    Toda parte de tradução funcionando!

--- core/admin.py
+++ core/admin.py
@@ -10,56 +10,62 @@ admin.site.site_title = 'Administração Currículo'
 admin.site.index_title = 'Faça suas alterações aqui'
 
 
-@admin.register(Service)
-class ServiceAdmin(admin.ModelAdmin):
-    list_display = ('servico', 'icone', 'ativo', 'modificado')
-
-
-# @admin.register(Education)
-# class EducationAdmin(admin.ModelAdmin):
-#     list_display = ('titulo', 'subtitulo', 'ativo', 'modificado')
-
-
-@admin.register(Experience)
-class ExperienceAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'subtitulo', 'ativo', 'modificado')
-
-
+# Não vou traduzir o endereço/ sem necessidade
 @admin.register(Endereco)
 class EnderecoAdmin(admin.ModelAdmin):
     list_display = ('cidade', 'ativo', 'modificado')
 
 
-@admin.register(Sobre)
-class SobreAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'ativo', 'modificado')
-
-
-@admin.register(Competencia)
-class CompetenciaAdmin(admin.ModelAdmin):
-    list_display = ('competencia', 'ativo', 'modificado')
-
-
+# Não precisa traduzir só possui uma imagem
 @admin.register(HeaderPicture)
 class HeaderAdmin(admin.ModelAdmin):
     list_display = ('foto', 'ativo', 'modificado')
 
 
+# Não precisa traduzir, só tem o gráfico e os programas
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
     list_display = ('nome', 'porcentagem', 'ativo', 'modificado')
 
 
+@admin.register(Education)
+class EducationAdmin(TranslationAdmin):
+    model = Education
+    list_display = ('titulo', 'subtitulo', 'ativo', 'modificado')
+
+
+@admin.register(Service)
+class ServiceAdmin(TranslationAdmin):
+    model = Service
+    list_display = ('servico', 'icone', 'ativo', 'modificado')
+
+
+@admin.register(Experience)
+class ExperienceAdmin(TranslationAdmin):
+    model = Experience
+    list_display = ('titulo', 'subtitulo', 'ativo', 'modificado')
+
+
+@admin.register(Sobre)
+class SobreAdmin(TranslationAdmin):
+    model = Sobre
+    list_display = ('nome', 'ativo', 'modificado')
+
+
+@admin.register(Competencia)
+class CompetenciaAdmin(TranslationAdmin):
+    model = Competencia
+    list_display = ('competencia', 'ativo', 'modificado')
+
+
 @admin.register(FiltrosProjetos)
-class SkillAdmin(admin.ModelAdmin):
+class FiltrosProjetosAdmin(TranslationAdmin):
+    model = FiltrosProjetos
     list_display = ('nome', 'ativo', 'modificado')
 
 
 @admin.register(Projetos)
-class SkillAdmin(admin.ModelAdmin):
+class ProjetosAdmin(admin.ModelAdmin):
+    model = Projetos
     list_display = ('nome', 'ativo', 'modificado')
 
-
-@admin.register(Education)
-class EducationAdmin(TranslationAdmin):
-    model = Education
--- core/translation.py
+++ core/translation.py
@@ -1,4 +1,4 @@
-from .models import Education
+from .models import Education, Service, Experience, Sobre, Competencia, FiltrosProjetos, Projetos
 
 from modeltranslation.translator import TranslationOptions, register
 
@@ -6,3 +6,33 @@ from modeltranslation.translator import TranslationOptions, register
 @register(Education)
 class EducationTranslationOptions(TranslationOptions):
     fields = ('titulo', 'subtitulo', 'descricao')
+
+
+@register(Service)
+class ServiceTranslationOptions(TranslationOptions):
+    fields = ('servico', 'descricao')
+
+
+@register(Experience)
+class ExperienceTranslationOpions(TranslationOptions):
+    fields = ('titulo', 'subtitulo', 'descricao')
+
+
+@register(Sobre)
+class SobreTranslationOpions(TranslationOptions):
+    fields = ('titulo', 'descricao', 'escolaridade')
+
+
+@register(Competencia)
+class CompetenciaTranslationOpions(TranslationOptions):
+    fields = ('competencia',)
+
+
+@register(FiltrosProjetos)
+class FiltrosProjetoTranslationOpions(TranslationOptions):
+    fields = ('nome',)
+
+
+@register(Projetos)
+class ProjetosTranslationOpions(TranslationOptions):
+    fields = ('nome',)
